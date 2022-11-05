fb56e7790f9077d02b0f75e564b1d0e129cdb66e
son <dawityitagesu@gmail.com>
2022-11-05 19:42:59 +0300

    post model

---
+++ lib/models/post.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Post {
+  String? id;
+  String title;
+  String body;
+  Post({this.id, required this.title,required this.body});
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+        id: json['id'].toString(),
+        title: json['name'].toString(),
+        body: json['email'].toString());
+  }
+}
