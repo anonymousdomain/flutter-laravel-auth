07e3b488b30777f56cbd88557432b0e798c71a54
son <dawityitagesu@gmail.com>
2022-11-05 19:43:32 +0300

    posts screen

--- lib/widgets/posts_screen.dart
+++ lib/widgets/posts_screen.dart
@@ -1,4 +1,11 @@
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:lara_fl/helper/dio.dart';
+import 'package:dio/dio.dart' as Dio;
+import '../models/post.dart';
 
 class PostsScreen extends StatefulWidget {
   const PostsScreen({super.key});
@@ -8,6 +15,18 @@ class PostsScreen extends StatefulWidget {
 }
 
 class _PostsScreenState extends State<PostsScreen> {
+  final storage = FlutterSecureStorage();
+
+  Future<List<Post>> getPosts() async {
+    final token = await storage.read(key: 'auth');
+    Dio.Response res = await dio().get('user/posts',
+        options: Dio.Options(headers: {
+          'Authorization': 'Bearer $token',
+        }));
+    List posts = res.data;
+    return posts.map((post) => Post.fromJson(post)).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +36,28 @@ class _PostsScreenState extends State<PostsScreen> {
         elevation: 0.0,
       ),
       body: Center(
-        child: Text('posts'),
+        child: FutureBuilder<List<Post>>(
+            future: getPosts(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                // log(snapshot.data!.first.body.toString());
+                return ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) {
+                      var item = snapshot.data![index];
+                      return ListTile(
+                        title: Text(item.title),
+                        subtitle: Text(item.body),
+                      );
+                    });
+              } else if (snapshot.hasError) {
+                log(snapshot.error.toString());
+                return Text('faild to load posts');
+              } else if (!snapshot.hasData) {
+                return Text('no data');
+              }
+              return CircularProgressIndicator();
+            }),
       ),
     );
   }
