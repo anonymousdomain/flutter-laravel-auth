8bb37d0a4c12a80607f6d3b6216ca81aa3bfca2e
anonymousdomain <dawityitagesu@gmail.com>
2022-11-19 19:16:24 +0300

    custom button

---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: must_be_immutable
+
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  CustomButton({Key? key, required this.onTap, required this.title})
+      : super(key: key);
+  Function() onTap;
+  String title;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8),
+      width: MediaQuery.of(context).size.width,
+      height: 80,
+      child: TextButton(
+          style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all<Color>(Colors.indigo),
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)))),
+          onPressed: onTap,
+          child: Text(title)),
+    );
+  }
+}
